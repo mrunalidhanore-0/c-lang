0109e1bb5c685b7056fe338ccef668ac153f8d4d
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-06-25 12:01:11 +0530

    Add files via upload

---
+++ prfirst/c lang.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int celsius,Fahrenheit;
+	printf("enter degree celsius:");
+	scanf("%d", &celsius);
+	Fahrenheit= 9/5 * celsius + 32;
+	
+	printf("the Fahrenheit is= %d ",Fahrenheit);
+	
+	
+}
