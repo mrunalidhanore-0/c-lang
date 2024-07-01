1044a23a865215188f3bb6011589dbf6818d9267
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-07-01 11:40:42 +0530

    Add files via upload

---
+++ ternary opertor.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int x,y;
+	printf("enter the number x:");
+	scanf("%d",&x);
+	printf("enter the number y:");
+	scanf("%d",&y);
+	
+	(x<y)? printf("the minimum value is x."):printf("the  minimum value is y.");}
