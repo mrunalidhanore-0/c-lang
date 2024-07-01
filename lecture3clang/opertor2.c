8f1e41e4deee13c16231a85847b94bb295bcd828
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-07-01 12:10:51 +0530

    Add files via upload

---
+++ lecture3clang/opertor2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y;
+	printf("enter the value of x:");
+	scanf("%d" ,&x);
+	printf("enter the value of y:");
+	scanf("%d" ,&y);
+	
+	printf ("(x-y)2 =%d",(x*x)-(2*x*y)+(y*y));
+}
---
+++ lecture3clang/opetor7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,z;
+	printf("enter the value of x:");
+	scanf("%d" ,&x);
+	printf("enter the value of y:");
+	scanf("%d" ,&y);
+	
+	printf ("(x+y+z)2 =%d",(x*x)+(y*y)+(z*z)+(2*(x*y)+(y*z)));}
