4c22bec0d28c5bb07719ffce29e4cf9154857289
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-07-02 09:59:19 +0530

    Add files via upload

---
+++ find the value clang/square.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float side,area;
+	
+	printf("enter the side of square:");
+	scanf("%d",&side);
+	
+	area=side*side;
+	printf("area of square=%.0f",area);
+	
+	
+}
