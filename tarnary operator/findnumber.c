03ae862f663db88ae10704679be71adbe92f2d06
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-07-02 12:18:01 +0530

    Add files via upload

---
+++ tarnary operator/findnumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	int x,y,z;
+	printf("enterx,y,and z:");
+	scanf("%d%d%d",&x,&y,&z);
+	
+	(x<y)?
+	(x<z)?
+	(y<z)? printf("x is minimum"):printf("z is minimum"):printf("y is minimum"):printf("z is minimum");
+	       
+}
---
+++ tarnary operator/switch case.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int choice,recharge;
+	
+	
+	printf("telecom call service..\n\n\n");
+	printf("press 1 for english\n");
+	printf("press 2 for hindi\n");
+	printf("press 3 for gujarati\n\n");
+	
+	printf("enter your choice :");
+	scanf("%d",&choice);
+	
+	
+	
+	switch(choice)
+	{
+		case 1:
+		  printf("press 1 for internet Recharge\n\n");
+		  printf("press 2 for Top-up Recharge\n\n");
+		  printf("press 3for special Recharge\n\n");
+		
+		
+		printf("enter your Recharge plan:");
+		scanf("%d",&choice);
+		
+	   switch(choice)
+	        case 1 :
+	        	printf("internet recharge is successful\n");
+	        break;	
+	        	printf("internet recharge is successful\n");
+	        break;	
+	        	printf("internet recharge is successful\n");
+		
+			switch(choice)
+					printf("Internet Recharge ke liye 1 dabaiye\n\n");
+            		printf("Top-up Recharge ke liya 2 dabaiye\n\n");
+					printf("special Recharge ke liya 3 dabaiye\n\n");
+					
+			printf("enter your Recharge plan\n\n");
+			scanf("%d",&choice);		 
+			
+			         
+		
+	}		
+}
