067c0f9a97dd395e141c56ecf1cec26bd0a09a7d
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-07-09 09:40:45 +0530

    Add files via upload

---
+++ if else.c.3.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+main ()
+
+{
+  int marks1,marks2,marks3,average;
+  printf("enter  marks of english (out of 100): \n ");
+  scanf("%d",&marks1);
+  printf("enter  marks of hindi (out of 100): \n ");
+  scanf("%d",&marks2);
+  printf("enter  marks of maths (out of 100): \n ");
+  scanf("%d",&marks3);
+  
+    average=(marks1+marks2+marks3)/3;
+	printf("the average is=%d",average);
+  
+  
+  
+  
+  
+}
