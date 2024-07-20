1873892205a3a41a9012b55fe2b586ffddca23fa
mrunalidhanore-0 <mrunalidhanore78@gmail.com>
2024-07-20 11:44:27 +0530

    Add files via upload

---
+++ que4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,n;
+	
+	printf("enter the value n:");
+	scanf("%d",&n);
+	
+	while(n>=i)
+	{
+		if(n%2==1)
+		{
+			printf("%d\t",n);
+			
+		}
+		
+		n--;
+	}
+}
