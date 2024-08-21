01b737f89f0aed43c7e22700dc75d5def98cdf55
mrunalidhanore07 <mrunalidhanore78@gmail.com>
2024-08-21 09:29:33 +0530

    Add files via upload

---
+++ prfirst/clang2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	 int salary =100 ,HRA=10, DA=5, TA=8;
+	 printf ("enter salary ");
+	 scanf("%d",& salary );
+}
---
+++ prfirst/pr2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main ()
+{
+	int angel1,angel2,angel3;
+	
+	printf("enter angel1:");
+	scanf("%d",&angel1);
+	
+	printf("enter angel2:");
+	scanf("%d",&angel2);
+	
+	angel3=180-angel1-angel2
+	
+}
