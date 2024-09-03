cafa2af8f9df59f29730df6d969cc33ccb75a069
mrunalidhanore07 <mrunalidhanore78@gmail.com>
2024-09-03 11:43:01 +0530

    Add files via upload

---
+++ loopingwithspace/q2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	
+	for(a=1; a<=5; a++)
+	{
+		for(c=5; c>=a; c--)
+		{
+			printf("  ");
+		}
+			for(b=a; b>=1; b--)
+			{
+				printf("%d ",b);
+			}
+			printf("\n");
+		
+	}
+	
+	
+}
+
---
+++ loopingwithspace/q3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	
+	for(a=5; a>=1; a--)
+	{
+		for(c=a; c>=1; c--)
+		{
+			printf("  ");
+		}
+			for(b=a; b<=5; b++)
+			{
+				printf("%d ",b);
+			}
+			printf("\n");
+		
+	}
+	
+	
+}
+
